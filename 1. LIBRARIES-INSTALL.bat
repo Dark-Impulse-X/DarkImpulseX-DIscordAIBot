473f519d10326ccda25458f16813bbe3c2544259
Shreejan Dolai <145888668+the-coder-boy@users.noreply.github.com>
2023-11-29 17:51:32 +0530

    Delete 1. LIBRARIES-INSTALL.bat

--- 1. LIBRARIES-INSTALL.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-start "" https://python.org/downloads
-pip install discord
-pip install openai
-pip install sys
-pip install os
-start "ADD DarkImpulseX TO YOUR SERVER" https://tinyurl.com/darkimpulsex
